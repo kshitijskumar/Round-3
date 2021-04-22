e6548a5c8bfbb6f56707b3d6a7434e2313a6ea5a
Kshitij Kumar <kshitij2212@gmail.com>
2021-04-22 15:12:07 +0530

    navigation component set

--- trell-android/app/src/main/java/com/example/trellandroid/MainActivity.kt
+++ trell-android/app/src/main/java/com/example/trellandroid/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.trellandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.trellandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navcontroller: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
     }
+
+    private fun setupNavigation() {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragmentContainer) as NavHostFragment
+        navcontroller = navHostFragment.navController
+    }
+
+
 }
